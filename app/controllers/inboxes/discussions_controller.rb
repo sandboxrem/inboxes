02e733fb6302f5991c6765917afb1106f6be0cf6
Andrii Zavadetskyi <andrii.y.z@gmail.com>
2013-03-22 23:50:58 -0500

    ability to respond to js

--- app/controllers/inboxes/discussions_controller.rb
+++ app/controllers/inboxes/discussions_controller.rb
@@ -27,16 +27,23 @@ class Inboxes::DiscussionsController < Inboxes::BaseController
 
   def create
     @discussion.add_recipient_token current_user.id
-
     @discussion.messages.each do |m|
       m.discussion = @discussion
       m.user = current_user
     end
-
-    if @discussion.save
-      redirect_to inboxes.discussion_url(@discussion), :notice => t("inboxes.discussions.started")
+    unless @discussion.save
+      respond_to do |format|
+        format.html { 
+          redirect_to inboxes.discussion_url(@discussion), 
+          :notice => t("inboxes.discussions.started")
+        }
+        format.js
+      end
     else
-      render :action => "new"
+      respond_to do |format|
+        format.html {render :action => "new" and return }
+        format.js
+      end  
     end
   end
 
@@ -45,18 +52,25 @@ class Inboxes::DiscussionsController < Inboxes::BaseController
   def load_and_check_discussion_recipient
     # initializing model for new and create actions
     @discussion = Discussion.new(params[:discussion].presence || {})
-
     # checking if discussion with this user already exists
-    if @discussion.recipient_ids && @discussion.recipient_ids.size == 1
+    if @discussion.recipient_ids && @discussion.recipient_ids.size == 1 &&
       user = User.find(@discussion.recipient_ids.first)
       discussion = Discussion.find_between_users(current_user, user)
       if discussion
         # it exists, let's add message and redirect current user
         @discussion.messages.each do |message|
-          Message.create(:discussion => discussion, :user => current_user, :body => message.body) if message.body
+          @message = Message.create(:discussion => discussion, :user => current_user, 
+           :body => message.body) if message.body
         end
         # redirecting to that existing object
-        redirect_to inboxes.discussion_url(discussion), :notice => t("inboxes.discussions.already_exists", :user => user[Inboxes::config.user_name])
+        respond_to do |format|
+          format.html {
+            redirect_to inboxes.discussion_url(discussion), 
+             :notice => t("inboxes.discussions.already_exists", 
+              :user => user[Inboxes::config.user_name]) 
+          }
+          format.js
+        end 
       end
     end
   end
